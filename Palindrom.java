520bb6fa3de62a8f02ea1e7e7ca618168474a8b1
Ahmet Coşkun Kızılkaya <70554333+ahmetk3436@users.noreply.github.com>
2022-11-12 03:59:26 +0300

    Create Palindrom.java

---
+++ Palindrom.java
@@ -0,0 +1,31 @@
+package manavKasaProgrami;
+
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) {
+		 System.out.println(isPolindrom(404));
+		 
+	    } 
+	 static boolean isPolindrom(int number){
+
+         int temp= number;
+         int reverseNumber=0,lastNumber;
+         while(temp !=0){
+             lastNumber=temp % 10;
+             reverseNumber=reverseNumber *10 +lastNumber;
+             temp /=10;
+         }
+
+         //System.out.println(reverseNumber);
+         //return true;
+
+        if(number==reverseNumber)
+            return true;
+        else
+            return false;
+
+     }
+	}
+
+	
